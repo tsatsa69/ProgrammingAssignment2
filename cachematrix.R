677114c2413725dbfb38c598e9a82b468d8c0fa9
Ivan Tsatsarov <ivan_tsatsarov@yahoo.com>
2016-10-09 01:19:20 +0300

    Programmin Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First function create matrix and allow it to be put in cache
+## Second function use first function to calculate inverse or get it from cache 
+## for testing :
+##  mtr<-makeCacheMatrix(matrix(rnorm(4000000),2000,2000))
+##  mt1<-cacheSolve(mtr) -- execute this 2 times to see difference in time
 
-## Write a short comment describing this function
+## Function create a matrix and allow its value to be stored in cache
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y)
+    {
+    x<<-y
+    m<<-NULL
+    }
+  get<-function() x
+  setinv<-function(inverse) m<<- inverse
+  getinv<-function() m
+  list(set=set, get=get,
+       setinv=setinv,
+       getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Accept matrix created from previous function and calculate inverse or take from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m<-x$getinv()
+  if(!is.null(m)){
+    message("getting cached data") #if matrix is in cache take from it
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setinv(m)
+  m
 }
